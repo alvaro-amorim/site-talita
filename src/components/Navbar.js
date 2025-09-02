fb11e1db2f85a6a88e95ad14f1f57eb7029ada7c
alvaro-amorim <alvaroamorimjf@gmail.com>
2025-09-02 12:15:45 -0300

    alterando navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
 // src/components/Navbar.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import logoTalita from '../assets/logo-talita.png';
-import corretoraImage from '../assets/corretora-talita.jpg';
 
 function Navbar() {
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    const handlePopState = () => {
+      setIsMenuOpen(false);
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [menuRef]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -16,7 +36,7 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar-brand-container">
+      <div className="navbar-logo-container">
         <Link to="/" className="navbar-logo-link">
           <img src={logoTalita} alt="Logo Talita Almeida" className="navbar-logo" />
         </Link>
@@ -28,7 +48,7 @@ function Navbar() {
         <span className="bar"></span>
       </button>
 
-      <div className={`nav-links-container ${isMenuOpen ? 'open' : ''}`}>
+      <div className={`nav-links-container ${isMenuOpen ? 'open' : ''}`} ref={menuRef}>
         <Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} onClick={toggleMenu}>HOME</Link>
         <Link to="/imoveis" className={`nav-link ${location.pathname === '/imoveis' ? 'active' : ''}`} onClick={toggleMenu}>Imóveis</Link>
         <Link to="/lancamentos" className={`nav-link ${location.pathname === '/lancamentos' ? 'active' : ''}`} onClick={toggleMenu}>Lançamentos</Link>
